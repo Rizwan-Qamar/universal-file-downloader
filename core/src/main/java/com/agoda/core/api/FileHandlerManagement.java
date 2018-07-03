c42d511a6bcdbab784944e544eda4a1907431c25
Rizwan <formanite07@gmail.com>
2018-07-03 23:19:03 +0900

    Complete Support for HTTP protocol
    Completed support for downloading files from HTTP protocol

--- core/src/main/java/com/agoda/core/api/FileHandlerManagement.java
+++ core/src/main/java/com/agoda/core/api/FileHandlerManagement.java
@@ -2,13 +2,19 @@ package com.agoda.core.api;
 
 import com.agoda.protocols.AbstractFileHandler;
 import com.agoda.protocols.HttpFileHandler;
+
+import java.io.IOException;
 import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Service;
 
 @Service
 @Scope("prototype")
 public class FileHandlerManagement implements com.agoda.core.interfaces.FileHandlerManagement {
+  private Logger log = LoggerFactory.getLogger(this.getClass());
 
   @Override
   public void downloadFiles(List<String> files) {
@@ -17,6 +23,16 @@ public class FileHandlerManagement implements com.agoda.core.interfaces.FileHand
     for (String file : files) {
       AbstractFileHandler abstractFileHandler = new HttpFileHandler();
       abstractFileHandler.setResourceLocation(file);
+      try {
+        abstractFileHandler.call();
+      } catch (IOException e) {
+        e.printStackTrace();
+        log.error(
+                String.format(
+                        "There was an exception for: %s",
+
+                        e.getMessage()));
+      }
     }
   }
 }
